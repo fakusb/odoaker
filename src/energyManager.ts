a79a724d95bc960389482970e6806d649322740f
Fabian Kunze <fakusb@fakusb.de>
2017-08-14 23:29:53 +0200

    add typings to tracked files

---
+++ src/energyManager.ts
@@ -0,0 +1,13 @@
+
+interface hasEnergy {
+    energy : number;
+    energyCapacity : number
+}
+
+interface hasStorage {
+    store : number;
+    storeCapacity : number
+}
+
+
+var test : hasStorage = {} as StructureContainer;
